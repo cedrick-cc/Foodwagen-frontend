b33e4583da9817c21ac1e1dc61e71efd7b456704
cedrick-cc <nkurunzizacedrick2@gmail.com>
2025-11-11 20:28:53 +0200

    added config files

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.js
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
